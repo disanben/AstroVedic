780fdfb32112517fe39bfbbf84135701e1972558
disanben <43569749+disanben@users.noreply.github.com>
2024-10-22 20:06:54 +0200

    Delete lancia_Carta_natale.bat

--- lancia_Carta_natale.bat
+++
@@ -1,8 +0,0 @@
-echo "Lancio dell'applicazione per calcolo della carta natale"
-
-SET NOME=%1
-SET CFG_PATH="..\\02-Cfg\\"
-SET INPUT_PATH="..\\11-DataInput\\"
-SET OUTPUT_PATH="..\\12-CarteNataliOutput\\"
-
-python cartanatale_01.py %INPUT_PATH%%NOME%.txt %CFG_PATH%Graha.cfg %CFG_PATH%Rasi.cfg %OUTPUT_PATH%Carta_Natale_%NOME%.txt 9
